ffa698d78b400131910efee0eae9e903905092ee
mihaela.radu <mihaela.radu@e3.co.uk>
2017-09-11 17:42:34 +0100

    Book listing /section done

---
+++ src/HMPPS.Models/Cms/Book.cs
@@ -0,0 +1,18 @@
+﻿using HMPPS.Models.Common;
+
+namespace HMPPS.Models.Cms
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Link Link { get; set; }
+        public Image Image { get; set; }
+        public bool OpenEreader { get; set; }
+
+        public Book()
+        {
+            Link = new Link();
+            Image = new Image();
+        }
+    }
+}
