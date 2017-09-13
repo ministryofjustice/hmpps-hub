bc8bbfbe4a48f6317681e6c59a479c4ee8cb21ef
Hannah <hannah.goodridge@e3.co.uk>
2017-09-13 17:44:53 +0100

    #6063 building neontroids

--- hmppsAssets/js/src/modules/neontroids.js
+++ hmppsAssets/js/src/modules/neontroids.js
@@ -1,7 +1,87 @@
 export default (function () {
   return {
     init: function init() {
-      
+      const neontroidsEl = document.querySelector('.js-neontroids');
+      const trigger = document.querySelector('.js-neontroids-trigger');
+
+      if (neontroidsEl) {
+        trigger.addEventListener('click', (e) => {
+          e.preventDefault();
+          HMPPS.neontroids.build();
+          const scripts = ['/hmppsAssets/js/src/third-party/lib/sound-fx.js', '/hmppsAssets/js/src/third-party/lib/keyboard-io.js', '/hmppsAssets/js/src/third-party/lib/collisions.js', '/hmppsAssets/js/src/third-party/lib/asteroids-sprites.js', '/hmppsAssets/js/src/third-party/lib/asteroids-polygon.js', '/hmppsAssets/js/src/third-party/lib/display-text.js', '/hmppsAssets/js/src/third-party/lib/asteroids.js'];
+
+          const loader = new HMPPS.neontroids.ScriptLoader();
+          let scriptPromises = scripts.map((file) => {
+            return loader.add(file);
+          });
+
+          Promise.all(scriptPromises).then( function (){
+
+            neontroidsEl.classList.add('neontroids-active');
+            document.body.classList.add('modal-active');
+
+            const root = document.getElementsByTagName('html')[0];
+            root.classList.add('modal-active');
+
+            const close = document.querySelector('.js-neontroids-close');
+            //close.focus();
+
+            close.addEventListener('click', () => {
+              HMPPS.neontroids.destroy();
+            });
+          });
+
+        });
+      }
+    },
+    build(){
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      const canvas = `<canvas id='canvas' class='canvas'></canvas>
+      <button class='js-neontroids-close neontroids__close' type='button' name='button'>Close</button>`;
+
+      modal.innerHTML = canvas;
+      document.body.appendChild(modal)
+    },
+    destroy(){
+      const modal = document.querySelector('.modal');
+      const body = document.body;
+      body.removeChild(modal);
+    },
+    ScriptLoader() {
+
+      const promises = [];
+
+      this.add = (url) => {
+        const promise = new Promise((resolve, reject) => {
+
+          const script = document.createElement('script');
+          script.src = url;
+          script.type = 'text/javascript';
+          script.async = true;
+          script.addEventListener('load', () => {
+            resolve(script);
+          }, false);
+          script.addEventListener('error', () => {
+            reject(script);
+            console.log('was rej');
+          }, false);
+
+          document.body.appendChild(script);
+        });
+
+        //promises.push(promise);
+        return promise;
+
+        console.log(promises);
+      };
+
+      this.loaded = (callbackOnFailed) => {
+        Promise.all(promises).then((result1) => {
+          console.log('Script loaded from:', result1);
+        }, callbackOnFailed);
+      };
     },
   };
 }());
