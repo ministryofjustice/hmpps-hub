e48977da5522bdda974735fcad4171521a62d8d1
Hannah <hannah.goodridge@e3.co.uk>
2017-09-29 09:46:14 +0100

    #6594 img path issue #6591 remove class from p tag

--- rawHTML/video.php
+++ rawHTML/video.php
@@ -23,7 +23,7 @@ include('_includes/head.php'); ?>
       </div>
       <div class="video-player">
         <video controls id="video" class="video-js custom-video vjs-default-skin" controls preload="auto">
-          <source src="../_dummy/video/dummy-mp4.mp4" type="video/mp4">
+          <source src="_dummy/video/dummy-mp4.mp4" type="video/mp4">
             <source src="http://digital-hub-dev.northeurope.cloudapp.azure.com:11002/sites/default/files/videos/2017-04/Timewise%20Intro%20V2%20%282%29.mp4" type="video/mp4">
               <div class="vjs-no-js">
                 <p>To listen to this audio please enable JavaScript, and consider upgrading to a web browser that
