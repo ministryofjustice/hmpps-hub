17ebf86077cc6cadf327248d7c853ffc28f9171d
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-09-29 10:50:02 +0100

    Cleaned up unnecessary references from HMPPS.Authentication + HMPPS.Authentication.Tests projects

--- src/HMPPS.Authentication/Settings.cs
+++ src/HMPPS.Authentication/Settings.cs
@@ -34,11 +34,5 @@ namespace HMPPS.Authentication
 
         public static string AuthenticationCheckerCookieKey => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.AuthenticationChecker.CookieKey");
 
-        public static string NomisApiBaseUrl => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.BaseUrl");
-
-        public static string NomisApiClientToken => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.ClientToken");
-
-        public static string NomisApiSecretKey => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.SecretKey");
-
     }
 }
