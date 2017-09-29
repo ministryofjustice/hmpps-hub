0fba4015841dc2a23d1c7c6aacd7e72355452dc0
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-09-29 11:33:10 +0100

    removed "Authentication" from Nomis config keys (HMPPS.Authentication.NomisApiService => HMPPS.NomisApiService)

--- src/HMPPS.NomisApi/HMPPS.NomisApiService/Settings.cs
+++ src/HMPPS.NomisApi/HMPPS.NomisApiService/Settings.cs
@@ -3,11 +3,11 @@ namespace HMPPS.NomisApiService
     public static class Settings
     {
 
-        public static string NomisApiBaseUrl => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.BaseUrl");
+        public static string NomisApiBaseUrl => Sitecore.Configuration.Settings.GetSetting("HMPPS.NomisApiService.BaseUrl");
 
-        public static string NomisApiClientToken => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.ClientToken");
+        public static string NomisApiClientToken => Sitecore.Configuration.Settings.GetSetting("HMPPS.NomisApiService.ClientToken");
 
-        public static string NomisApiSecretKey => Sitecore.Configuration.Settings.GetSetting("HMPPS.Authentication.NomisApiService.SecretKey");
+        public static string NomisApiSecretKey => Sitecore.Configuration.Settings.GetSetting("HMPPS.NomisApiService.SecretKey");
 
     }
 }
