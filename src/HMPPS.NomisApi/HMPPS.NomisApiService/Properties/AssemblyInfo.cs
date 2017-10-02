f24d39e77767861083d30baddbfd25c409bfc38d
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-10-02 17:03:52 +0100

    Renamed NomisApiService.dll -> HMPPS.NomisApiService.dll

--- src/HMPPS.NomisApi/HMPPS.NomisApiService/Properties/AssemblyInfo.cs
+++ src/HMPPS.NomisApi/HMPPS.NomisApiService/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NomisApiService")]
+[assembly: AssemblyTitle("HMPPS.NomisApiService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NomisApiService")]
+[assembly: AssemblyProduct("HMPPS.NomisApiService")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
