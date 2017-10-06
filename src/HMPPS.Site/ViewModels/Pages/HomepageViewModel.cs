02420b79fb5e88cf10a0f0bc730b967a2b2637f8
mihaela.radu <mihaela.radu@e3.co.uk>
2017-10-06 16:11:10 +0100

    Context Sensitive Profile Unilink for the Prisoner. FE fixes integration.

--- src/HMPPS.Site/ViewModels/Pages/HomepageViewModel.cs
+++ src/HMPPS.Site/ViewModels/Pages/HomepageViewModel.cs
@@ -4,5 +4,7 @@ namespace HMPPS.Site.ViewModels.Pages
 {
     public class HomepageViewModel : BaseViewModel
     {
+        public bool ShowQuickLinks { get; set; }
+        public string SelfHelpLinkUrl { get; set; }
     }
 }
