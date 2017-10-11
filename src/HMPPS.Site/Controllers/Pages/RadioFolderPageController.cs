b4625e0a04b3333b47c2c7e932029f9629158b82
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-10-11 17:40:58 +0100

    Added Prev / Next links to Audio file listing page

--- src/HMPPS.Site/Controllers/Pages/RadioFolderPageController.cs
+++ src/HMPPS.Site/Controllers/Pages/RadioFolderPageController.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Sitecore.Data.Items;
 using HMPPS.Models.Cms;
 using HMPPS.Site.Controllers.Base;
 using HMPPS.Site.ViewModels.Pages;
-using System.Linq;
+using HMPPS.Site.ViewModels.Partials;
+using System;
+using Sitecore.Globalization;
+using HMPPS.Models.Common;
 
 namespace HMPPS.Site.Controllers.Pages
 {
@@ -19,7 +23,29 @@ namespace HMPPS.Site.Controllers.Pages
         {
             _rfpvm = new RadioFolderPageViewModel();
 
+            _rfpvm.PrevNext = BuildPrevNextViewModel(contextItem);
+
             _rfpvm.BreadcrumbItems = BreadcrumbItems;
+
+        }
+
+        private PrevNextViewModel BuildPrevNextViewModel(Item contextItem)
+        {
+            var prevNext = new PrevNextViewModel();
+            var siblings = contextItem.Parent.Children.ToList();
+
+            var currentItemIndex = siblings.FindIndex(i => i.ID.Guid.Equals(contextItem.ID.Guid));
+
+            var prevItem = currentItemIndex <= 0 ? null : siblings[currentItemIndex - 1];
+            var nextItem = currentItemIndex >= siblings.Count - 1 ? null : siblings[currentItemIndex + 1];
+
+            prevNext.PreviousLink = BuildLink(prevItem);
+            prevNext.NextLink = BuildLink(nextItem);
+
+            prevNext.PreviousText = Translate.Text("Previous");
+            prevNext.NextText = Translate.Text("Next");
+
+            return prevNext;
         }
 
         private List<RadioFolder> PopulateFolderList(Item parent, Item current)
