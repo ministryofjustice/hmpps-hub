b5a646dc1e1033a5d49456030ba893671ccdbded
Hannah <hannah.goodridge@e3.co.uk>
2017-10-13 15:59:51 +0100

    #6954 video player layout adjustments

--- rawHTML/videos-one.php
+++ rawHTML/videos-one.php
@@ -19,21 +19,21 @@ include('_includes/head.php'); ?>
       <div class="grid-row">
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">All Kinds of Minds</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Art That Gives You A Voice</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Becoming a non-smoker</h2>
             </a>
           </div>
@@ -42,21 +42,21 @@ include('_includes/head.php'); ?>
       <div class="grid-row">
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Body of Health</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Dealing with Criticism</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Documentaries</h2>
             </a>
           </div>
@@ -65,21 +65,21 @@ include('_includes/head.php'); ?>
       <div class="grid-row">
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Electrical Engineering</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Emotional Hygiene</h2>
             </a>
           </div>
         </div>
         <div class="column-one-third">
           <div class="image-grid-item">
-            <a href=".php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
+            <a href="video.php"><img src="https://dummyimage.com/300x200/eaeaea/000" alt="">
             <h2 class="heading-medium">Category</h2>
             </a>
           </div>
