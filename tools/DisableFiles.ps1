1b96bfc2688de0d3cc328484751856fd7b5d7e5f
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-10-26 09:50:00 +0100

    Added Unicorn folder removal, No errors if files don't exist

--- tools/DisableFiles.ps1
+++ tools/DisableFiles.ps1
@@ -1,33 +1,36 @@
-$wwwAppConfigInclude = "D:\home\site\wwwroot\App_Config\Include"
-$wwwAdminLocation = "D:\home\site\wwwroot\sitecore\admin"
+$wwwRoot = "D:\home\site\wwwroot"
+$wwwAppConfigInclude = "$wwwRoot\App_Config\Include"
+$wwwAdminLocation = "$wwwRoot\sitecore\admin"
 
-ren "$wwwAppConfigInclude\Sitecore.WebDAV.config" "Sitecore.WebDAV.config.disabled"
+if ([IO.File]::Exists("$wwwAppConfigInclude\Sitecore.WebDAV.config")) { ren "$wwwAppConfigInclude\Sitecore.WebDAV.config" "Sitecore.WebDAV.config.disabled" }
 
-ren "$wwwAdminLocation\Cache.aspx" "Cache.aspx.disabled"
-ren "$wwwAdminLocation\DBCleanup.aspx" "DBCleanup.aspx.disabled"
-ren "$wwwAdminLocation\dbbrowser.aspx" "dbbrowser.aspx.disabled"
-ren "$wwwAdminLocation\ShowServicesConfig.aspx" "ShowServicesConfig.aspx.disabled"
-ren "$wwwAdminLocation\eventqueuestats.aspx" "eventqueuestats.aspx.disabled"
-ren "$wwwAdminLocation\FillDB.aspx" "FillDB.aspx.disabled"
-ren "$wwwAdminLocation\InstallLanguage.aspx" "InstallLanguage.aspx.disabled"
-ren "$wwwAdminLocation\Jobs.aspx" "Jobs.aspx.disabled"
-ren "$wwwAdminLocation\LinqScratchPad.aspx" "LinqScratchPad.aspx.disabled"
-ren "$wwwAdminLocation\Logs.aspx" "Logs.aspx.disabled"
-ren "$wwwAdminLocation\MediaHash.aspx" "MediaHash.aspx.disabled"
-ren "$wwwAdminLocation\PackageItem.aspx" "PackageItem.aspx.disabled"
-ren "$wwwAdminLocation\PathAnalyzer.aspx" "PathAnalyzer.aspx.disabled"
-ren "$wwwAdminLocation\Pipelines.aspx" "Pipelines.aspx.disabled"
-ren "$wwwAdminLocation\PublishQueueStats.aspx" "PublishQueueStats.aspx.disabled"
-ren "$wwwAdminLocation\RawSearch.aspx" "RawSearch.aspx.disabled"
-ren "$wwwAdminLocation\RebuildKeyBehaviorCache.aspx" "RebuildKeyBehaviorCache.aspx.disabled"
-ren "$wwwAdminLocation\RebuildReportingDB.aspx" "RebuildReportingDB.aspx.disabled"
-ren "$wwwAdminLocation\RedeployMarketingData.aspx" "RedeployMarketingData.aspx.disabled"
-ren "$wwwAdminLocation\RemoveBrokenLinks.aspx" "RemoveBrokenLinks.aspx.disabled"
-ren "$wwwAdminLocation\restore.aspx" "restore.aspx.disabled"
-ren "$wwwAdminLocation\SecurityTools.aspx" "SecurityTools.aspx.disabled"
-ren "$wwwAdminLocation\serialization.aspx" "serialization.aspx.disabled"
-ren "$wwwAdminLocation\SetSACEndpoint.aspx" "SetSACEndpoint.aspx.disabled"
-ren "$wwwAdminLocation\ShowConfig.aspx" "ShowConfig.aspx.disabled"
-ren "$wwwAdminLocation\SqlShell.aspx" "SqlShell.aspx.disabled"
-ren "$wwwAdminLocation\stats.aspx" "stats.aspx.disabled"
-ren "$wwwAdminLocation\unlock_admin.aspx" "unlock_admin.aspx.disabled"
+if ([IO.File]::Exists("$wwwAdminLocation\Cache.aspx")) { ren "$wwwAdminLocation\Cache.aspx" "Cache.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\DBCleanup.aspx")) { ren "$wwwAdminLocation\DBCleanup.aspx" "DBCleanup.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\dbbrowser.aspx")) { ren "$wwwAdminLocation\dbbrowser.aspx" "dbbrowser.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\ShowServicesConfig.aspx")) { ren "$wwwAdminLocation\ShowServicesConfig.aspx" "ShowServicesConfig.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\eventqueuestats.aspx")) { ren "$wwwAdminLocation\eventqueuestats.aspx" "eventqueuestats.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\FillDB.aspx")) { ren "$wwwAdminLocation\FillDB.aspx" "FillDB.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\InstallLanguage.aspx")) { ren "$wwwAdminLocation\InstallLanguage.aspx" "InstallLanguage.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\Jobs.aspx")) { ren "$wwwAdminLocation\Jobs.aspx" "Jobs.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\LinqScratchPad.aspx")) { ren "$wwwAdminLocation\LinqScratchPad.aspx" "LinqScratchPad.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\Logs.aspx")) { ren "$wwwAdminLocation\Logs.aspx" "Logs.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\MediaHash.aspx")) { ren "$wwwAdminLocation\MediaHash.aspx" "MediaHash.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\PackageItem.aspx")) { ren "$wwwAdminLocation\PackageItem.aspx" "PackageItem.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\PathAnalyzer.aspx")) { ren "$wwwAdminLocation\PathAnalyzer.aspx" "PathAnalyzer.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\Pipelines.aspx")) { ren "$wwwAdminLocation\Pipelines.aspx" "Pipelines.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\PublishQueueStats.aspx")) { ren "$wwwAdminLocation\PublishQueueStats.aspx" "PublishQueueStats.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\RawSearch.aspx")) { ren "$wwwAdminLocation\RawSearch.aspx" "RawSearch.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\RebuildKeyBehaviorCache.aspx")) { ren "$wwwAdminLocation\RebuildKeyBehaviorCache.aspx" "RebuildKeyBehaviorCache.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\RebuildReportingDB.aspx")) { ren "$wwwAdminLocation\RebuildReportingDB.aspx" "RebuildReportingDB.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\RedeployMarketingData.aspx")) { ren "$wwwAdminLocation\RedeployMarketingData.aspx" "RedeployMarketingData.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\RemoveBrokenLinks.aspx")) { ren "$wwwAdminLocation\RemoveBrokenLinks.aspx" "RemoveBrokenLinks.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\restore.aspx")) { ren "$wwwAdminLocation\restore.aspx" "restore.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\SecurityTools.aspx")) { ren "$wwwAdminLocation\SecurityTools.aspx" "SecurityTools.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\serialization.aspx")) { ren "$wwwAdminLocation\serialization.aspx" "serialization.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\SetSACEndpoint.aspx")) { ren "$wwwAdminLocation\SetSACEndpoint.aspx" "SetSACEndpoint.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\ShowConfig.aspx")) { ren "$wwwAdminLocation\ShowConfig.aspx" "ShowConfig.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\SqlShell.aspx")) { ren "$wwwAdminLocation\SqlShell.aspx" "SqlShell.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\stats.aspx")) { ren "$wwwAdminLocation\stats.aspx" "stats.aspx.disabled" }
+if ([IO.File]::Exists("$wwwAdminLocation\unlock_admin.aspx")) { ren "$wwwAdminLocation\unlock_admin.aspx" "unlock_admin.aspx.disabled" }
+
+Remove-Item "$wwwRoot\App_Data\Unicorn" -Recurse -ErrorAction Ignore
