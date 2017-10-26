e326761b675e6bc4640ec4f336212e8d0272bb0b
mihaela.radu <mihaela.radu@e3.co.uk>
2017-10-26 11:36:26 +0100

    Move secrets to app settings

--- src/HMPPS.MediaLibrary/HMPPS.MediaLibrary.CloudStorage/Configuration/Settings.cs
+++ src/HMPPS.MediaLibrary/HMPPS.MediaLibrary.CloudStorage/Configuration/Settings.cs
@@ -1,12 +1,13 @@
 using HMPPS.MediaLibrary.CloudStorage.Constants;
 using Sitecore;
+using System.Configuration;
 using SC = Sitecore.Configuration;
 
 namespace HMPPS.MediaLibrary.CloudStorage.Configuration
 {
     public class Settings
     {
-        public static string CloudMediaUrl => StringUtil.EnsurePostfix('/', SC.Settings.GetSetting(ConfigSettings.MediaLinkCdnServerUrl));
+        public static string CloudMediaUrl => StringUtil.EnsurePostfix('/', ConfigurationManager.AppSettings[ConfigSettings.MediaLinkCdnServerUrl]);
 
         public static string MediaThumbnailCacheFolder => StringUtil.EnsurePostfix('/', SC.Settings.GetSetting(ConfigSettings.MediaThumbnailCacheFolder, "/App_Data/MediaThumbnailCache"));
 
