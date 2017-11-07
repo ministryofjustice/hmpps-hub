b9bc2d2eff357c8f410edd2fa807bacd9b76f97b
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-11-07 17:26:40 +0000

    Removed unnecessary changes of LoginRedirector.cs

--- src/HMPPS.Authentication/HMPPS.Authentication/Pipelines/LoginRedirector.cs
+++ src/HMPPS.Authentication/HMPPS.Authentication/Pipelines/LoginRedirector.cs
@@ -1,22 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using Sitecore;
 using Sitecore.Pipelines.HttpRequest;
 using Sitecore.Sites;
 using Sitecore.Web;
 using IdentityModel.Client;
 using HMPPS.Utilities.Helpers;
-using System.Globalization;
-using HMPPS.Utilities.Models;
-using System.Web;
-using HMPPS.Utilities.Services;
-using Sitecore.Security.Authentication;
 
 namespace HMPPS.Authentication.Pipelines
 {
-    public class LoginRedirector : HttpRequestProcessor 
+    public class LoginRedirector : HttpRequestProcessor
     {
         public override void Process(HttpRequestArgs args)
         {
@@ -25,10 +18,6 @@ namespace HMPPS.Authentication.Pipelines
             if ((new[] { "shell", "login", "admin" }).Contains(Context.Site.Name)) return;
             // force login only for normal website usage, not for preview / debugging / experienceediting / profiling
             if (!Context.PageMode.IsNormal) return;
-
-            // TODO: remove hardcoded user when IDAM is back
-            LogInHardcodedIdamUser(args.Context);
-
 
             if (Context.User.IsAuthenticated) return;
             if (!SiteManager.CanEnter(Context.Site.Name, Context.User)) return;
@@ -57,37 +46,6 @@ namespace HMPPS.Authentication.Pipelines
 
                 // Redirect the user to the login page of the identity provider
                 WebUtil.Redirect(url);
-            }
-        }
-
-        private void LogInHardcodedIdamUser(HttpContext context)
-        {
-            if (!Context.User.IsAuthenticated)
-            {
-                List<Claim> claims = new List<Claim>();
-                claims.Add(new Claim(ClaimTypes.NameIdentifier, "A1412AE"));
-                claims.Add(new Claim(ClaimTypes.GivenName, "James"));
-                claims.Add(new Claim(ClaimTypes.Surname, "Bond"));
-                claims.Add(new Claim(ClaimTypes.Email, "james.bond@example.com"));
-                claims.Add(new Claim("name", "James Bond"));
-                claims.Add(new Claim("access_token", ""));
-                claims.Add(new Claim("refresh_token", ""));
-                claims.Add(new Claim("expires_at", ExpirationHelper.GetExpirationTimeString(86400)));
-                claims.Add(new Claim("prison_id", "ISI"));
-                claims.Add(new Claim("prison_name", "Sheffield Prison"));
-                claims.Add(new Claim("account_balance", "123.40"));
-                claims.Add(new Claim("account_balance_lastupdated", DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)));
-
-                var userData = new UserData(claims);
-                var _jwtTokenService = new JwtTokenService();
-                var _encryptionService = new EncryptionService();
-                var _userDataService = new UserDataService(_encryptionService, _jwtTokenService);
-                _userDataService.SaveUserDataToCookie(claims, context);
-
-                var sitecoreUser = BuildVirtualUser(userData);
-                AuthenticationManager.LoginVirtualUser(sitecoreUser);
-
-                WebUtil.Redirect(context.Request.Url.AbsoluteUri);
             }
         }
     }
