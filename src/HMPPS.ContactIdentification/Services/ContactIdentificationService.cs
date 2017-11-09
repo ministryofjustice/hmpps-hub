f8ea33a46779fbcd618d4bcad2a54098aa19c06a
Eva Den-Boros <eva.denboros@e3.co.uk>
2017-11-09 14:56:49 +0000

    Fixed PR comments

--- src/HMPPS.ContactIdentification/Services/ContactIdentificationService.cs
+++ src/HMPPS.ContactIdentification/Services/ContactIdentificationService.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using Sitecore.Analytics;
 using Sitecore.Analytics.Model;
 using Sitecore.Security.Accounts;
@@ -16,10 +14,17 @@ namespace HMPPS.ContactIdentification.Services
             var contactId = Tracker.Current?.Contact?.Identifiers?.Identifier;
             if (string.IsNullOrEmpty(contactId) || contactId != Sitecore.Context.User.LocalName)
             {
-                Tracker.Initialize();
-                Tracker.Current.Contact.Identifiers.AuthenticationLevel = AuthenticationLevel.PasswordValidated;
-                Tracker.Current.Session.Identify(Sitecore.Context.User.LocalName);
-                SetContactFacets(Sitecore.Context.User);
+                try
+                {
+                    Tracker.Initialize();
+                    Tracker.Current.Contact.Identifiers.AuthenticationLevel = AuthenticationLevel.PasswordValidated;
+                    Tracker.Current.Session.Identify(Sitecore.Context.User.LocalName);
+                    SetContactFacets(Sitecore.Context.User);
+                }
+                catch
+                {
+                    Sitecore.Diagnostics.Log.Error("HMPPS.ContactIdentification.Services.ContactIdentificationService - Error in IdentifyTrackerContact()", this);
+                }
             }
         }
 
@@ -28,37 +33,6 @@ namespace HMPPS.ContactIdentification.Services
             IContactPersonalInfo personalFacet = Tracker.Current.Contact.GetFacet<IContactPersonalInfo>("Personal");
             personalFacet.FirstName = string.Empty;
             personalFacet.Surname = user.LocalName;
-
-            //// Removed real FirstName, LastName and email from contact facets
-            //// TODO: delete codes if not needed
-            
-            //personalFacet.FirstName = GetFirstName(user.Profile.FullName);
-            //personalFacet.Surname = GetSurName(user.Profile.FullName);
-            //IContactEmailAddresses addressesFacet = Tracker.Current.Contact.GetFacet<IContactEmailAddresses>("Emails");
-            //IEmailAddress address;
-            //if (!addressesFacet.Entries.Contains("work_email"))
-            //{
-            //    address = addressesFacet.Entries.Create("work_email");
-            //    address.SmtpAddress = user.Profile.Email;
-            //    addressesFacet.Preferred = "work_email";
-            //}
-            
         }
-
-        //// Removed real FirstName, LastName and email from contact facets
-        //// TODO: delete codes if not needed
-
-        //private static string GetFirstName(string fullname)
-        //{
-        //    if (fullname.Contains(' '))
-        //        return fullname.Substring(0, fullname.IndexOf(" "));
-        //    return fullname;
-        //}
-        //private static string GetSurName(string fullname)
-        //{
-        //    if (fullname.Contains(' '))
-        //        return fullname.Substring(fullname.IndexOf(" ") + 1);
-        //    return String.Empty;
-        //}
     }
 }
