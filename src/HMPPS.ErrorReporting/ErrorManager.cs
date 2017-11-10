3287a486db2303f765f0ca9ca32947fe545ba623
mihaela-radu <mihaela.radu@e3.co.uk>
2017-11-10 17:32:11 +0000

    Error reporting WIP

---
+++ src/HMPPS.ErrorReporting/ErrorManager.cs
@@ -0,0 +1,29 @@
+using System;
+using Sitecore.Diagnostics;
+
+namespace HMPPS.ErrorReporting
+{
+    public class ErrorManager : IErrorManager
+    {
+        public void LogInfo(string message, Type sourceType)
+        {
+            Log.Info(message, sourceType);
+        }
+        public void LogAudit(string message, Type sourceType)
+        {
+            Log.Audit(message, sourceType);
+        }
+        public void LogDebug(string message, Type sourceType)
+        {
+            Log.Debug(message, sourceType);
+        }
+        public void LogWarning(string message, Type sourceType)
+        {
+            Log.Warn(message, sourceType);
+        }
+        public void LogError(string message, Exception ex, Type sourceType)
+        {
+            Log.Error(message, ex, sourceType);
+        }
+    }
+}
