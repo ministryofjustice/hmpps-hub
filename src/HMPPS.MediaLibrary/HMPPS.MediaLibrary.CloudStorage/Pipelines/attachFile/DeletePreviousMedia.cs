06f59f0ba3422177231878fa11abf35a97acbd89
Jack Durcan <jack.durcan@e3.co.uk>
2017-11-21 11:33:11 +0000

    Rolled out use of injected log manager across remaining sitecore log uses

--- src/HMPPS.MediaLibrary/HMPPS.MediaLibrary.CloudStorage/Pipelines/attachFile/DeletePreviousMedia.cs
+++ src/HMPPS.MediaLibrary/HMPPS.MediaLibrary.CloudStorage/Pipelines/attachFile/DeletePreviousMedia.cs
@@ -6,6 +6,8 @@ using HMPPS.MediaLibrary.CloudStorage.Provider;
 using Sitecore.Diagnostics;
 using Sitecore.Pipelines.Attach;
 using Sitecore.StringExtensions;
+using HMPPS.ErrorReporting;
+using HMPPS.Utilities.Helpers;
 
 namespace HMPPS.MediaLibrary.CloudStorage.Pipelines.AttachFile
 {
@@ -14,11 +16,13 @@ namespace HMPPS.MediaLibrary.CloudStorage.Pipelines.AttachFile
     /// </summary>
     public class DeletePreviousMedia
     {
-        ICloudStorageProvider cloudStorage;
+        private ICloudStorageProvider _cloudStorage;
+        private ILogManager _logManager;
 
         public DeletePreviousMedia()
         {
-            cloudStorage = new CloudStorageProvider();
+            _cloudStorage = new CloudStorageProvider();
+            _logManager = DependencyInjectionHelper.ResolveService<ILogManager>();
         }
 
         /// <summary>
@@ -32,10 +36,10 @@ namespace HMPPS.MediaLibrary.CloudStorage.Pipelines.AttachFile
             if (!args.MediaItem.FileBased)
                 return;
 
-            Log.Audit("HMPPS.MediaLibrary.CloudStorage.Pipelines.AttachFile.DeletePreviousMedia - Deleting '{0}' from Cloud storage".FormatWith(args.MediaItem.FilePath), this);
+            _logManager.LogAudit("HMPPS.MediaLibrary.CloudStorage.Pipelines.AttachFile.DeletePreviousMedia - Deleting '{0}' from Cloud storage".FormatWith(args.MediaItem.FilePath), GetType());
 
             PipelineHelper.AddContainerNameToArgs(args,  GetContainerNameFromFilePath(args.MediaItem.FilePath));
-            cloudStorage.Delete(args.MediaItem);
+            _cloudStorage.Delete(args.MediaItem);
         }
 
         private string GetContainerNameFromFilePath(string filePath)
