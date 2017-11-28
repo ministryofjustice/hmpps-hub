499e687013d64cdc7e438cfb50e6b910c4a78ebd
mihaela-radu <mihaela.radu@e3.co.uk>
2017-11-28 12:47:24 +0000

    Add Sitecore PowerShell Extensions-4.7 package files to source control

---
+++ src/HMPPS.Site/sitecore modules/PowerShell/Scripts/VariablePrompt.js
@@ -0,0 +1,22 @@
+﻿jQuery(document).ready(function($) {
+    $("#Copyright").each(function() { // Notice the .each() loop, discussed below
+        $(this).qtip({
+            content: {
+                text: "Copyright &copy; 2010-2017 Adam Najmanowicz, Michael West. All rights Reserved.\r\n",
+                title: "Sitecore PowerShell Extensions"
+            },
+            position: {
+                my: "bottom left",
+                at: "top center"
+            },
+            style: {
+                width: 355,
+                "max-width": 355
+            },
+            hide: {
+                event: false,
+                inactive: 3000
+            }
+        });
+    });
+});
---
+++ src/HMPPS.Site/sitecore modules/PowerShell/Scripts/listview.js
@@ -0,0 +1,21 @@
+﻿(function($, window, cognifide, undefined) {
+
+    cognifide.powershell.updateStatusBarCounters = function (itemCount, currentPage, pageCount) {
+        $("#ItemCount").text(itemCount);
+        $("#CurrentPage").text(currentPage);
+        $("#PageCount").text(pageCount);
+    }
+
+    $(function () {
+        window.parent.focus();
+        window.focus();
+
+        $("#Input_Filter").keypress(function(event) {
+            if (event.which === 13) {
+                event.preventDefault();
+                window.scForm.postRequest("", "", "", "pslv:filter");
+            };
+        });
+    });
+
+}(jQuery, window, window.cognifide = window.cognifide || {}));
