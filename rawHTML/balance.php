09a18e9d88945d149ab0737f44242ec00c734051
HEBRON\hannah.localadmin <hannah.goodridge@e3.co.uk>
2018-01-15 14:38:42 +0000

    #9451 error message

--- rawHTML/balance.php
+++ rawHTML/balance.php
@@ -13,6 +13,7 @@ include('_includes/head.php'); ?>
   </div>
   <div class="pub-c-title">
     <h1 class="heading-xlarge">Check Balances</h1>
+    <h2 class="heading-large">Error msg here</h2>
   </div>
   <div class="at-a-glance">
     <div class="grid-row">
