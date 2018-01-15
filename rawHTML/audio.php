8c7d35bdbe73e6d219fa4a5934fe361ba1bba66c
HEBRON\hannah.localadmin <hannah.goodridge@e3.co.uk>
2018-01-15 12:57:25 +0000

    #9429 removed chevron

--- rawHTML/audio.php
+++ rawHTML/audio.php
@@ -35,7 +35,7 @@ include('_includes/head.php'); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
       <div class="more-link">
-        <h2 class="heading-large"><a href="#">Show previous episodes ></a></h2>
+        <h2 class="heading-large"><a href="#">Show previous episodes</a></h2>
       </div>
     </div>
     <div class="column-third add-title-margin">
