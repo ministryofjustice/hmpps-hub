a08fb678a7c0eb12d9ffedbc9d5eb2ab592753fe
Jack Durcan <jack.durcan@e3.co.uk>
2018-01-19 17:50:32 +0000

    Centralised health check logic into its own project

---
+++ src/HMPPS.Monitoring/HMPPS.HealthCheck/HealthCheckFacet.cs
@@ -0,0 +1,11 @@
+namespace HMPPS.HealthCheck
+{
+    public class HealthCheckFacet
+    {
+        public string Name { get; set; }
+
+        public bool Healthy { get; set; }
+
+        public string Details { get; set; }
+    }
+}
